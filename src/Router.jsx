b7ad270798de50dc0bd163f0913735d1d1bb7b8b
Rahman <fadhllulrahman687@gmail.com>
2026-01-30 13:35:01 +0700

    feat:add admin dashboard(incomplete)

--- src/Router.jsx
+++ src/Router.jsx
@@ -2,12 +2,16 @@ import { Routes, Route } from "react-router";
 import Home from "./pages/Home";
 import MainLayout from "./components/layouts/MainLayout";
 import { Login } from "./pages/Login";
+import AdminDashboard from "./pages/AdminDashboard";
 
 export default function Router() {
   return (
     <Routes>
       <Route path="/" element={<MainLayout />}>
         <Route index element={<Home />} />
+        <Route path="admin/">
+          <Route index element={<AdminDashboard />} />
+        </Route>
       </Route>
       <Route path="login" element={<Login />} />
       <Route
