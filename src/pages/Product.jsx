0552fa16db3715c84d6660c5ddcc91c5e421524d
Muhammad Bian Al Baihaqi <bianalbaihaqi190@gmail.com>
2026-02-01 00:28:38 +0700

    fix[Search layout] Remove search layout

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -2,12 +2,13 @@ import { useState, useRef, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchProducts, setPage } from "../redux/slices/product.slice";
 import ProductList from "../components/ui/product/ProductList";
-import MobileSearch from "../components/ui/product/MobileSearch";
 import BgCoffee from "../assets/product/bg-coffee.jpg";
 import ArrowLeft from "../assets/home/arrow-left.png";
 import ArrowRight from "../assets/home/arrow-right.png";
 import Animate from "../assets/product/animate.svg";
 import Animate2 from "../assets/product/animate2.svg";
+import Filter from "../assets/product/Filter.svg";
+
 
 export default function Product() {
   const dispatch = useDispatch();
@@ -101,7 +102,35 @@ export default function Product() {
   return (
     <>
       {/* Mobile Search & Filter Bar - Only visible on mobile/tablet */}
-      <MobileSearch search={search} setSearch={setSearch} />
+      <section className="px-4 pt-6 lg:hidden">
+        <div className="flex gap-3">
+          <div className="relative flex-1">
+            <input
+              type="text"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+              placeholder="Find Product"
+              className="h-10 w-full rounded-lg border border-gray-100 bg-white px-4 pl-10 text-sm shadow-sm focus:outline-none"
+            />
+            <svg
+              className="absolute top-1/2 left-3 h-4 w-4 -translate-y-1/2 text-gray-400"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              ></path>
+            </svg>
+          </div>
+          <button className="bg-brand-orange rounded-lg p-2.5 text-white shadow-sm">
+            <img src={Filter} alt="Filter" />
+          </button>
+        </div>
+      </section>
 
       {/* Hero Banner Section - Hidden on Mobile */}
       <section className="hidden overflow-hidden lg:relative lg:block">
