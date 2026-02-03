2d11ec34cf723fb2063a76adf1b19965a48fc0e8
Virgil I Ambar <imanuelambar11@gmail.com>
2026-02-04 03:50:33 +0700

    feat: edit product detail

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,59 +1,59 @@
 import { Routes, Route } from "react-router";
-import Home from "./pages/Home";
 import MainLayout from "./components/layouts/MainLayout";
-import { Login } from "./pages/Login";
-import ProductLayout from "./components/layouts/ProductLayout";
-import Product from "./pages/Product";
-import Register from "./pages/Register";
-import { ForgotPassword } from "./pages/ForgotPassword";
-import OrderLayout from "./components/layouts/OrderLayout";
-import DetailOrder from "./pages/DetailOrder";
-import AdminDashboard from "./pages/AdminDashboard";
 import DashboardLayout from "./components/layouts/DashboardLayout";
-import HistoryOrder from "./pages/HistoryOrder";
-import AdminUserlist from "./pages/AdminUserlist";
+
+import Home from "./pages/Home";
+import Product from "./pages/Product";
 import ProductDetail from "./pages/ProductDetail";
 import CheckoutProduct from "./pages/CheckoutProduct";
 import Profile from "./pages/Profile";
-import ProfileLayout from "./components/layouts/ProfileLayout";
+import DetailOrder from "./pages/DetailOrder";
+import HistoryOrder from "./pages/HistoryOrder";
+
+import { Login } from "./pages/Login";
+import Register from "./pages/Register";
+import { ForgotPassword } from "./pages/ForgotPassword";
+
+import AdminDashboard from "./pages/AdminDashboard";
+import AdminUserlist from "./pages/AdminUserlist";
 
 export default function Router() {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<MainLayout />}>
-          <Route index element={<Home />} />
-        </Route>
-        <Route path="dashboard" element={<DashboardLayout />}>
-          <Route path="admin">
-            <Route index element={<AdminDashboard />} />
-            <Route path="users-list" element={<AdminUserlist />} />
-          </Route>
-        </Route>
-        <Route path="product" element={<ProductLayout />}>
-          <Route index element={<Product />} />
-          <Route path="detail-product" element={<ProductDetail />} />
-          <Route path="checkout-product" element={<CheckoutProduct />} />
-        </Route>
-        <Route path="order" element={<OrderLayout />}>
-          <Route path="detail" element={<DetailOrder />} />
-          <Route path="history" element={<HistoryOrder />} />
-        </Route>
-        <Route path="profile" element={<ProfileLayout />}>
-          <Route index element={<Profile />} />
-        </Route>
-        <Route path="login" element={<Login />} />
-        <Route path="register" element={<Register />} />
-        <Route path="forgot-password" element={<ForgotPassword />} />
-        <Route
-          path="*"
-          element={
-            <div className="flex h-dvh items-center justify-center text-5xl font-bold">
-              404 NOT FOUND
-            </div>
-          }
-        />
-      </Routes>
-    </>
+    <Routes>
+
+      {/*Semua halaman user pakai 1 layout */}
+      <Route element={<MainLayout />}>
+        <Route path="/" element={<Home />} />
+
+        <Route path="product" element={<Product />} />
+        <Route path="product/:id" element={<ProductDetail />} />
+        <Route path="checkout-product" element={<CheckoutProduct />} />
+
+        <Route path="order/detail" element={<DetailOrder />} />
+        <Route path="order/history" element={<HistoryOrder />} />
+
+        <Route path="profile" element={<Profile />} />
+      </Route>
+
+      {/* Dashboard admin layout terpisah */}
+      <Route path="dashboard" element={<DashboardLayout />}>
+        <Route path="admin" element={<AdminDashboard />} />
+        <Route path="admin/users-list" element={<AdminUserlist />} />
+      </Route>
+
+      {/* Auth tanpa navbar */}
+      <Route path="login" element={<Login />} />
+      <Route path="register" element={<Register />} />
+      <Route path="forgot-password" element={<ForgotPassword />} />
+
+      <Route
+        path="*"
+        element={
+          <div className="flex h-dvh items-center justify-center text-5xl font-bold">
+            404 NOT FOUND
+          </div>
+        }
+      />
+    </Routes>
   );
 }
